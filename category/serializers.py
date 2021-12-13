47fb49ed2442267a8ee9fa4df5d054997a7ed9b7
Paul Okoli <paulokoli7@gmail.com>
2021-12-13 13:18:27 +0100

    Bookstore E-commerce API

--- category/serializers.py
+++ category/serializers.py
@@ -1,9 +1,7 @@
-from rest_framework import serializers
+from rest_framework import fields, serializers
 from .models import Category
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category 
-        
-    def representation(self, instance):
-        return instance.name
+        fields = ['name', 'description']
