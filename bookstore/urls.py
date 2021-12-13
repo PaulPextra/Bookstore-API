20b90e0866484e5c2a1cb7a6c5c37c585e23ab6e
Paul Okoli <paulokoli7@gmail.com>
2021-12-13 11:26:15 +0100

    Bookstore E-commerce API

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -3,6 +3,7 @@ from . import views
 from . models import Book
 
 urlpatterns = [
+    path('books/add/', views.add_book),
     path('books/', views.book_list ),
     path('books/<int:book_id>/', views.search_by_id),
     path('books/<str:category>/', views.search_by_category),
--- bookstore/views.py
+++ bookstore/views.py
@@ -3,6 +3,43 @@ from . serializers import BookSerializer, BookDetailSerializer
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAdminUser
+from drf_yasg.utils import swagger_auto_schema
+
+@swagger_auto_schema(method='POST', request_body=BookSerializer())
+@api_view(['POST'])
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAdminUser])
+def add_book(request):
+    
+    if request.method == 'POST':
+        
+        serializer_class = BookSerializer(data=request.data)
+        
+        if serializer_class.is_valid():
+            
+            book_obj = Book.objects.create(**serializer_class.validated_data)
+            
+            serializer = BookSerializer(book_obj)
+        
+            context = {
+                'status': True,
+                'message': 'Success',
+                'data': serializer.data
+            }
+        
+            return Response(context, status=status.HTTP_201_CREATED)
+
+        else:
+            context = {
+                'status': False,
+                'message': 'Failed',
+                'data': serializer_class.errors
+            }
+        
+            return Response(context, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 def book_list(request):
